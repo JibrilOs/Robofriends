74bf2b9cd2cb854f07f8aaba9913c6499be9d265
unknown <webxite23@gmail.com>
2020-10-19 23:31:03 +0300

    i changed class app component to function component and i was able to use use react hook in there to make usestate and useEffect

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,51 +1,43 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "./App.css";
 import Cardlist from "../Component/Cardlist";
 
 import Scroll from "../Component/Scroll";
 import Search from "../Component/Search";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      robots: [],
-      searchField: "",
-    };
-  }
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+
+
+function App(){
+
+const[robots,setRobots]=useState([]);
+const[searchField,setSearchField]=useState("");
+
+ useEffect(()=>{fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => {
         return response.json();
       })
       .then((users) => {
-        this.setState({ robots: users });
-      });
-  }
+        setRobots(users);
+      });},[]);// the second parameter [] means run only once if there is a change in the state
 
-  // this a syntehtic event listener
-  handleChange = (e) => {
+  const handleChange = (e) => {
     // property.value allows us to get hold of what the person is typing
 
-    this.setState({ searchField: e.target.value }, () => {
-      console.log(this.state);
-    });
+   setSearchField( e.target.value );
   };
-
-  render() {
-    const filteredRobots = this.state.robots.filter((rob) => {
+const filteredRobots = robots.filter((rob) => {
       return (
-        rob.name.toLowerCase().includes(this.state.searchField.toLowerCase()) ||
-        rob.email.toLowerCase().includes(this.state.searchField.toLowerCase())
+        rob.name.toLowerCase().includes(searchField.toLowerCase()) ||
+        rob.email.toLowerCase().includes(searchField.toLowerCase())
       ); //the condition in the filter array is that bring me the names of if what we type in the search can be found in the robot data
     });
-    if (this.state.robots.length === 0) {
+    if (robots.length === true) {
       return <h1 className="tc"> Loading</h1>;
     } else {
       return (
         <div className="App">
           <h1>ROBOFRIENDS</h1>
-          <Search search={this.handleChange} />
+          <Search search={handleChange} />
           <Scroll>
             {/* over here we are wrapping cardlist component    we can do this cus we need to use this method called props.children in the scroll component */}
             <Cardlist robot={filteredRobots} />
@@ -53,7 +45,61 @@ class App extends Component {
         </div>
       );
     }
-  }
+
+
+
+
+
 }
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       robots: [],
+//       searchField: "",
+//     };
+//   }
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((users) => {
+//         this.setState({ robots: users });
+//       });
+//   }
+
+//   // this a syntehtic event listener
+//   handleChange = (e) => {
+//     // property.value allows us to get hold of what the person is typing
+
+//     this.setState({ searchField: e.target.value }, () => {
+//       console.log(this.state);
+//     });
+//   };
+
+//   render() {
+//     const filteredRobots = this.state.robots.filter((rob) => {
+//       return (
+//         rob.name.toLowerCase().includes(this.state.searchField.toLowerCase()) ||
+//         rob.email.toLowerCase().includes(this.state.searchField.toLowerCase())
+//       ); //the condition in the filter array is that bring me the names of if what we type in the search can be found in the robot data
+//     });
+//     if (this.state.robots.length === 0) {
+//       return <h1 className="tc"> Loading</h1>;
+//     } else {
+//       return (
+//         <div className="App">
+//           <h1>ROBOFRIENDS</h1>
+//           <Search search={this.handleChange} />
+//           <Scroll>
+//             {/* over here we are wrapping cardlist component    we can do this cus we need to use this method called props.children in the scroll component */}
+//             <Cardlist robot={filteredRobots} />
+//           </Scroll>
+//         </div>
+//       );
+//     }
+//   }
+// }
 
 export default App;
